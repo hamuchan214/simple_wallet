fe28f9bac92ddb2fb00d9f6825de02598e6564dc
solithv <120559288+solithv@users.noreply.github.com>
2025-01-07 13:15:32 +0900

    feature: add transaction with tag view

---
+++ backend/prisma/createView.sql
@@ -0,0 +1,7 @@
+DROP VIEW IF EXISTS transaction_with_tag;
+
+CREATE VIEW transaction_with_tag AS
+    SELECT tr.id as transaction_id, ta.id as tag_id, tr.user_id, tr.amount, tr.description, tr.date, ta.name as tag_name
+    FROM tags_on_transaction tt
+    JOIN "transaction" tr ON tt.transaction_id = tr.id
+    JOIN tag ta ON tt.tag_id = ta.id;
