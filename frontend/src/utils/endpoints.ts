da52213f73e49e995bd6a4a4c71b459dcd204448
solithv <120559288+solithv@users.noreply.github.com>
2025-01-21 16:08:57 +0900

    fix: remove base url

--- frontend/src/utils/endpoints.ts
+++ frontend/src/utils/endpoints.ts
@@ -1,11 +1,9 @@
-const BASE_URL: string = import.meta.env.VITE_API_URL || ``;
-
 const requests = {
-  login: `${BASE_URL}/login`,
-  register: `${BASE_URL}/register`,
-  transactions: `${BASE_URL}/transactions`,
-  statistics: `${BASE_URL}/statistics`,
-  tags: `${BASE_URL}/tags`,
+  login: `/login`,
+  register: `/register`,
+  transactions: `/transactions`,
+  statistics: `/statistics`,
+  tags: `/tags`,
 };
 
-export default requests; 
+export default requests;
