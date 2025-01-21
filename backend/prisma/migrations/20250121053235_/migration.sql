e14ff563acaaabe5f53eb024fece1d508b841aa9
solithv <120559288+solithv@users.noreply.github.com>
2025-01-21 14:47:18 +0900

    chore: remove obsolete migration files for database schema cleanup

--- backend/prisma/migrations/20250121053235_/migration.sql
+++ backend/prisma/migrations/20250121053235_/migration.sql
@@ -1,19 +1,52 @@
 /*
   Warnings:
 
-  - You are about to drop the `tag` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `tags_on_transaction` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Tag` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `TagsOnTransactions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Transaction` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- DropTable
 PRAGMA foreign_keys=off;
-DROP TABLE "tag";
+DROP TABLE "Tag";
 PRAGMA foreign_keys=on;
 
 -- DropTable
 PRAGMA foreign_keys=off;
-DROP TABLE "tags_on_transaction";
+DROP TABLE "TagsOnTransactions";
 PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Transaction";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "User";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "transaction" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "description" TEXT,
+    "date" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "transaction_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
 
 -- CreateTable
 CREATE TABLE "system_tag" (
@@ -41,7 +74,7 @@ CREATE TABLE "system_tags_on_transaction" (
     "updated_at" DATETIME NOT NULL,
 
     PRIMARY KEY ("transaction_id", "tag_id"),
-    CONSTRAINT "system_tags_on_transaction_transaction_id_fkey" FOREIGN KEY ("transaction_id") REFERENCES "transaction" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "system_tags_on_transaction_transaction_id_fkey" FOREIGN KEY ("transaction_id") REFERENCES "transaction" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "system_tags_on_transaction_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "system_tag" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
@@ -53,9 +86,15 @@ CREATE TABLE "custom_tags_on_transaction" (
     "updated_at" DATETIME NOT NULL,
 
     PRIMARY KEY ("transaction_id", "tag_id"),
-    CONSTRAINT "custom_tags_on_transaction_transaction_id_fkey" FOREIGN KEY ("transaction_id") REFERENCES "transaction" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "custom_tags_on_transaction_transaction_id_fkey" FOREIGN KEY ("transaction_id") REFERENCES "transaction" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "custom_tags_on_transaction_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "custom_tag" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- CreateIndex
+CREATE INDEX "transaction_user_id_idx" ON "transaction"("user_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "system_tag_name_key" ON "system_tag"("name");
