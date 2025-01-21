f279c6a2a564d54882da67b55cfd1422a9a17365
solithv <120559288+solithv@users.noreply.github.com>
2025-01-21 16:07:39 +0900

    fix: use axios instance with baseURL

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,28 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
+import { logout } from "../lib/localStorage";
 
-const axiosInstance = axios.create({
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  }
+const BASE_URL: string = import.meta.env.VITE_API_URL || ``;
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // トークンが無効な場合はログアウト
+    if (error.response?.status === 401) {
+      logout();
+      // ログイン画面へリダイレクト
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
 
 // トークンを設定するための関数
 export const setAuthToken = (token: string) => {
   if (token) {
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    localStorage.setItem('token', token);
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    localStorage.setItem("token", token);
   } else {
-    delete axiosInstance.defaults.headers.common['Authorization'];
-    localStorage.removeItem('token');
+    delete axiosInstance.defaults.headers.common["Authorization"];
+    localStorage.removeItem("token");
   }
 };
 
 // 起動時に保存されているトークンを設定
-const token = localStorage.getItem('token');
+const token = localStorage.getItem("token");
 if (token) {
   setAuthToken(token);
 }
 
-export default axiosInstance; 
+export default axiosInstance;
