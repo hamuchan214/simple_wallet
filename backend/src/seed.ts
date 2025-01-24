93e3794051247b8ed18bc58722308ac0a0794e6c
solithv <120559288+solithv@users.noreply.github.com>
2025-01-24 13:44:02 +0900

    feature: add tag type

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -3,37 +3,37 @@ import { prisma } from "./configs/prisma";
 const main = async () => {
   const systemTagNames = [
     // 収入カテゴリ
-    { name: "給与" },
-    { name: "賞与" },
-    { name: "副収入" },
-    { name: "投資収入" },
-    { name: "臨時収入" },
+    { name: "給与", type: "income" },
+    { name: "賞与", type: "income" },
+    { name: "副収入", type: "income" },
+    { name: "投資収入", type: "income" },
+    { name: "臨時収入", type: "income" },
 
     // 固定費カテゴリ
-    { name: "住居費" },
-    { name: "水道光熱費" },
-    { name: "通信費" },
-    { name: "保険料" },
-    { name: "ローン返済" },
+    { name: "住居費", type: "expense" },
+    { name: "水道光熱費", type: "expense" },
+    { name: "通信費", type: "expense" },
+    { name: "保険料", type: "expense" },
+    { name: "ローン返済", type: "expense" },
 
     // 変動費カテゴリ
-    { name: "食費" },
-    { name: "日用品" },
-    { name: "交通費" },
-    { name: "医療費" },
-    { name: "衣服費" },
+    { name: "食費", type: "expense" },
+    { name: "日用品", type: "expense" },
+    { name: "交通費", type: "expense" },
+    { name: "医療費", type: "expense" },
+    { name: "衣服費", type: "expense" },
 
     // 娯楽・趣味カテゴリ
-    { name: "外食" },
-    { name: "娯楽費" },
-    { name: "旅行" },
-    { name: "書籍" },
-    { name: "習い事" },
+    { name: "外食", type: "expense" },
+    { name: "娯楽費", type: "expense" },
+    { name: "旅行", type: "expense" },
+    { name: "書籍", type: "expense" },
+    { name: "習い事", type: "expense" },
 
     // その他
-    { name: "教育費" },
-    { name: "贈答費" },
-    { name: "雑費" },
+    { name: "教育費", type: "expense" },
+    { name: "贈答費", type: "expense" },
+    { name: "雑費", type: "expense" },
   ];
 
   const systemTags = await prisma.$transaction(async (tx) => {
@@ -43,15 +43,15 @@ const main = async () => {
           await tx.systemTag.upsert({
             where: { name: tag.name },
             update: {},
-            create: {
-              name: tag.name,
-            },
+            create: { name: tag.name, type: tag.type },
           })
       )
     );
   });
 
-  console.log(systemTags.map((tag) => ({ id: tag.id, name: tag.name })));
+  console.log(
+    systemTags.map((tag) => ({ id: tag.id, name: tag.name, type: tag.type }))
+  );
 };
 
 main()
